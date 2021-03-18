18e530bd2c401156ffe4dede5556304312bcde12
Hayden Garrard <hagarrard@gmail.com>
2021-03-18 08:17:55 -0500

    file structure set up

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inq = require("inquirer");
+const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -12,5 +12,5 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  mainMenu();
+  startMenu();
 });
