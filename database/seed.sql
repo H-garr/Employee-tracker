17182cfd1e5bd241fd48eef0ce23483d194c5edc
Hayden Garrard <hagarrard@gmail.com>
2021-03-18 09:36:02 -0500

    added a seed

---
+++ database/seed.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO department (name) VALUES ("Marketing Team");
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Management");
+INSERT INTO department (name) VALUES ("Art Team");
+INSERT INTO department (name) VALUES ("Secretary");
+
+INSERT INTO role (title, salary) VALUES ("Head of Marketing",50000);
+INSERT INTO role (title, salary) VALUES ("Sales Person",35000);
+INSERT INTO role (title, salary) VALUES ("General Manager",70000);
+INSERT INTO role (title, salary) VALUES ("Junior Artist",25000);
+INSERT INTO role (title, salary) VALUES ("Senior Artist",45000);
+INSERT INTO role (title, salary) VALUES ("Secretary",20000);
+
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Don","Draper",1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Pete","Campbell",2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Roger","Sterling",3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Peggy","Olson",4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Salvatore","Romano",4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Joan","Holloway",5);
+
